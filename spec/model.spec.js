85f4373c5af57f3da3e3863f66a1b9b56020ae08
Bob Yexley <bob@yexley.net>
2014-02-28 13:17:12 -0500

    Beginnings

--- spec/model.spec.js
+++ spec/model.spec.js
@@ -1,5 +1,22 @@
-var assert = require("assert");
+var assert = require("chai").assert;
+var expect = require("chai").expect;
+var should = require("chai").should;
+
+var Model = require("../src/model");
+var metadata = require("./model.spec.metadata");
+
+var model;
 
 describe("Model", function () {
-    it("should not allow instantiation without a metadata file");
+    beforeEach(function () {
+        model = new Model(metadata);
+    });
+
+    afterEach(function () {
+        model = null;
+    });
+
+    it("should not allow instantiation without a metadata object", function () {
+        expect(Model).to.throw(Error);
+    });
 });
